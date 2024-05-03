a35a0091507d88461324a6afc47d0d7aa2dc5e5e
Matt Bremmers <bremmdev@gmail.com>
2024-05-03 10:40:39 +0200

    add poll form validation

--- src/app/_actions/session.ts
+++ src/app/_actions/session.ts
@@ -7,7 +7,7 @@ export async function createPokerSession() {
   redirect(`/session/${sessionId}`);
 }
 
-export async function createPoll(formdata: FormData) {
+export async function createPoll(previousState: any, formdata: FormData) {
   const pollId = crypto.randomUUID().replace(/-/g, "");
 
   const data = Object.fromEntries(formdata.entries());
@@ -20,6 +20,11 @@ export async function createPoll(formdata: FormData) {
   const filteredData = Object.fromEntries(
     Object.entries(data).filter(([key, value]) => value !== "")
   );
+
+  //needs to have at least 2 answer options
+  if (Object.keys(filteredData).length < 3) {
+    return { error: "At least 2 answer options are required" };
+  }
 
   const partyURL = process.env.NEXT_PUBLIC_PARTY_HOST
     ? `https://${process.env.NEXT_PUBLIC_PARTY_HOST}`
--- src/components/Poll.tsx
+++ src/components/Poll.tsx
@@ -3,11 +3,14 @@
 import React from "react";
 import { createPoll } from "@/app/_actions/session";
 import SubmitButton from "@/components/SubmitButton";
+import { useFormState } from "react-dom";
 
 const Poll = () => {
   const [question, setQuestion] = React.useState("");
   const [showPollForm, setShowPollForm] = React.useState(false);
   const [answerCount, setAnswerCount] = React.useState(2);
+
+  const [state, formAction] = useFormState(createPoll, null);
 
   return (
     <section aria-label="poll" className="space-y-4">
@@ -30,11 +33,11 @@ const Poll = () => {
 
       {showPollForm && (
         <form
-          action={createPoll}
+          action={formAction}
           className="animate-fadeIn bg-orange-50 p-8 rounded-lg border border-orange-600 space-y-4"
         >
           <label htmlFor="question" className="blocktext-slate-800 font-medium">
-            Title
+            Question
           </label>
           <input
             type="text"
@@ -54,26 +57,28 @@ const Poll = () => {
               >
                 Answer {index + 1}
               </label>
-              <div className="flex gap-2">
-                <input
-                  type="text"
-                  id={`answer-${index + 1}`}
-                  className="bg-slate-50 w-full rounded-md border h-10 border-slate-600 bg-inherit px-4 py-2 placeholder:text-slate-400 placeholder:text-sm placeholder:italic focus:border-orange-600 focus:outline-none"
-                  name={`answer-${index + 1}`}
-                  placeholder={`Enter answer ${index + 1}`}
-                />
-                {index === answerCount - 1 && (
-                  <button
-                    onClick={() => setAnswerCount(answerCount + 1)}
-                    className="w-fit items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-white transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-orange-500 text-slate-950 hover:bg-orange-500/80 h-10 px-4 py-2 flex gap-2"
-                  >
-                    Add Answer
-                  </button>
-                )}
-              </div>
+
+              <input
+                type="text"
+                id={`answer-${index + 1}`}
+                className="bg-slate-50 w-full rounded-md border h-10 border-slate-600 bg-inherit px-4 py-2 placeholder:text-slate-400 placeholder:text-sm placeholder:italic focus:border-orange-600 focus:outline-none"
+                name={`answer-${index + 1}`}
+                placeholder={`Enter answer ${index + 1}`}
+              />
+              {index === answerCount - 1 && (
+                <button
+                  onClick={() => setAnswerCount(answerCount + 1)}
+                  className="my-4 w-fit items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-white transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-white border border-orange-600 text-slate-950 hover:bg-slate-100 h-10 px-4 py-2 flex gap-2"
+                >
+                  Add Answer Option
+                </button>
+              )}
             </div>
           ))}
 
+          {state?.error && (
+            <p className="text-rose-500 font-bold my-4">{state.error}</p>
+          )}
           <SubmitButton type="poll" />
         </form>
       )}
